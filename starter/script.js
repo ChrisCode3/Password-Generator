37526673381995a7c710995c04d347fe8162aa84
ChrisCode3 <malusdarkblade66@gmail.com>
2023-01-27 18:26:14 +0000

    reverting to previous commit

--- starter/script.js
+++ starter/script.js
@@ -1,44 +1,86 @@
 
 
-
 function generatePassword() {
   var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-  var  uppercaseCharacters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-  var  lowercaseCharacters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-  var  specialCharacters = ['@', '%', '+', '\\', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.'];
-  var  possibleCharacters = [];
+  var upperCasedCharacters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+  var lowerCasedCharacters= ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+  var specialCharacters = ['@', '%', '+', '\\', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.'];
+  var possibleCharacters = [];
+
+
+
+
+  var generateBtn = document.querySelector("#generate");
+
+
+
+  // Function to prompt user for password options
+  function getPasswordOptions() {
+
+  }
+
+  // Function for getting a random element from an array
+  function getRandom(arr) {
+
+  }
+
+  // Function to generate password with user input
+  function generatePassword() {
+
+  }
+
+  // Get references to the #generate element
+  var generateBtn = document.querySelector('#generate');
+
+  // Write password to the #password input
+  function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector('#password');
+
+    passwordText.value = password;
+  }
+
+
+
+  // Add event listener to generate button
+  generateBtn.addEventListener('click', writePassword);
+
+
 
 
   numberOfCharacters = prompt("How many characters do you want in your password? Choose between 8-128 characters.");
   if (numberOfCharacters < 8 || numberOfCharacters > 128) {
-    return "Please choose a valid number of characters.";
   } else if (isNaN(numberOfCharacters)) {
     numberOfCharacters = prompt("Please enter a valid number.");
   }
   else {
     alert("Your password will be " + numberOfCharacters + " characters long.");
-    
+
   }
 
-  hasLowercase = confirm("Do you want lowercase characters?");
-  if (hasLowercase) {
+  yesLowercase = confirm("Do you want lowercase characters?");
+  if (yesLowercase) {
     var turnToLowercase = alert("Your password will have lowercase characters.");
   }
   else {
     alert("Your password will NOT have lowercase characters.");
   }
 
-  hasUppercase = confirm("Do yu want uppercase characters?");
-  if (hasUppercase) {
+
+  yesUppercase = confirm("Do you want uppercase characters?");
+  if (yesUppercase) {
     alert("Your password will have uppercase characters.");
   }
   else {
     alert("Your password will NOT have uppercase characters.");
   }
 
-  hasNumbers = confirm("Do you want to use numbers?");
-  if (hasNumbers) {
+
+
+  isNumbers = confirm("Do you want to use numbers?");
+  if (isNumbers) {
     alert("Your password will have numbers.");
+
   }
   else {
     alert("Your password will NOT have numbers.");
@@ -52,29 +94,30 @@ function generatePassword() {
     alert("Your password will NOT have special characters.");
   }
 
-  if (hasLowercase === false && hasUppercase === false && hasNumbers === false && hasSpecial === false) {
-    return "Please select at least one character typ.";
-  };
+  let characters = [];
 
+  if (yesLowercase) {
+    characters.push(lowerCasedCharacters);
+  }
 
-  if (hasLowercase) {
-    possibleCharacters = possibleCharacters.concat(lowercaseCharacters);
+  if (yesUppercase) {
+    characters.push(upperCasedCharacters);
   }
-  if (hasUppercase) {
-    possibleCharacters = possibleCharacters.concat(uppercaseCharacters);
+  if (isNumbers) {
+    characters.push(numericCharacters);
   }
-  if (hasNumbers) {
-    possibleCharacters = possibleCharacters.concat(numericCharacters);
-  }
+
   if (hasSpecial) {
-    possibleCharacters = possibleCharacters.concat(specialCharacters);
+    characters.push(specialCharacters);
   }
+
+
 
   // pick random cards out of new pool for length of password
   let finalPassword = ""
   for (let i = 0; i < numberOfCharacters; i++) {
-    let rng =[Math.floor(Math.random() * possibleCharacters.length)];
-    
+    let rng = [Math.floor(Math.random() * possibleCharacters.length)];
+    // or finalPassword += possibleCharacters[rng];
     finalPassword = finalPassword + possibleCharacters[rng];
   }
   return finalPassword;
@@ -92,3 +135,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
